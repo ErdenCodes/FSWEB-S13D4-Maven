3c8bbdec6bf443f7cff7941729d42d0a9b79a649
ErdenCodes <erden.codes@gmail.com>
2024-11-05 00:21:42 +0300

    s13d4 bitti
    Ödev bitti

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,15 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        // İki Point nesnesi oluştur
+        Point p1 = new Point(3,4); // İlk nokta
+        Point p2 = new Point(0, 0); // İkinci nokta
+
+        // İki nokta arasındaki mesafeyi hesapla
+        double mesafe = p1.distance(p2); // p1 ve p2 arasındaki mesafeyi hesaplar
+
+        // Mesafeyi ekrana yazdır
+        System.out.println("İki nokta arasındaki mesafe: " + mesafe);
     }
+
 }
